dbb4a3b0d63786c6e1a7cbeb9f6d3dbc705d91f2
palsatyam <91319280+palsatyam@users.noreply.github.com>
2024-01-01 10:40:28 +0530

    initial commit --#01012024

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Builderz - Construction Company Website Template</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Construction Company Website Template" name="keywords">
+        <meta content="Construction Company Website Template" name="description">
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon">
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+        <!-- CSS Libraries -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="lib/flaticon/font/flaticon.css" rel="stylesheet"> 
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="lib/slick/slick.css" rel="stylesheet">
+        <link href="lib/slick/slick-theme.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="wrapper">
+
+            <!-- Top Bar Start -->
+            <?php include_once(__DIR__."/util/topbar.php")?>
+            <!-- Top Bar End -->
+
+            <!-- Nav Bar Start -->
+            <?php include_once(__DIR__."/util/nav.php")?>
+            <!-- Nav Bar End -->
+
+            <!-- Carousel Start -->
+            <?php include_once(__DIR__."/util/carousal.php")?>
+            <!-- Carousel End -->
+
+            <!-- Feature Start-->
+            <?php include_once(__DIR__."/includes/features.php")?>
+            <!-- Feature End-->
+
+            <!-- About Start -->
+            <?php include_once(__DIR__."/includes/about.php")?>
+            <!-- About End -->
+
+            <!-- Fact Start -->
+            <?php include_once(__DIR__."/includes/fact.php")?>
+            <!-- Fact End -->
+
+            <!-- Service Start -->
+            <?php include_once(__DIR__."/includes/services.php")?>
+            <!-- Service End -->
+
+            <!-- Video Start -->
+            <?php include_once(__DIR__."/includes/video.php")?>
+            <!-- Video End -->
+
+            <!-- Team Start -->
+            <?php include_once(__DIR__."/includes/team.php")?>   
+            <!-- Team End -->
+
+            <!-- FAQs Start -->
+            <?php include_once(__DIR__."/includes/faqs.php")?>
+            <!-- FAQs End -->
+
+            <!-- customer rating blog Start -->
+            <?php include_once(__DIR__."/includes/customerratingblog.php")?>
+            <!-- customer rating blog End -->
+
+            <!-- Blog Start -->
+            <?php include_once(__DIR__."/includes/blog.php"); ?>
+            <!-- Blog End -->
+
+            <!-- footer Starts -->
+            <?php include_once(__DIR__."/util/footer.php"); ?>
+            <!-- footer End -->
+
+            <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+        </div>
+
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="lib/lightbox/js/lightbox.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/counterup/counterup.min.js"></script>
+        <script src="lib/slick/slick.min.js"></script>
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
